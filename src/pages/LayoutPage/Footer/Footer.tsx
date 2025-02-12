4a694a5d7a73c261172d9edf4f4c2542a84cc72c
nbmendez <nbmendez@ebay.com>
2025-02-12 18:29:41 -0300

    change map to german

--- src/pages/LayoutPage/Footer/Footer.tsx
+++ src/pages/LayoutPage/Footer/Footer.tsx
@@ -40,11 +40,11 @@ const Footer = () => {
         <iframe 
           className="office-location-map"
           title="office-location-map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2594.9464292269304!2d8.683177699999998!3d49.428826799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4797c140a24a26c7%3A0x5fb1f404ac324294!2sDr.%20med.%20Carmen%20M%C3%BCller!5e0!3m2!1ses!2sar!4v1734554542611!5m2!1ses!2sar"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2594.9464292269304!2d8.683177699999998!3d49.428826799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4797c140a24a26c7%3A0x5fb1f404ac324294!2sDr.%20med.%20Carmen%20M%C3%BCller!5e0!3m2!1sde!2dde!4v1734554542611!5m2!1sde!2dde"
           allowFullScreen
           loading="lazy" 
           referrerPolicy="no-referrer-when-downgrade">
-          </iframe>
+        </iframe>
       </div>
     </div>
   );
