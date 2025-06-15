3b8c12cf7682151d35c5cd05d35bcefc2260164b
Nicolas Beltran <nicobeltran92@gmail.com>
2025-06-16 01:09:25 +0200

    Add integration with google doc

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -45,68 +45,25 @@ const Popup = (props: Props) => {
   const { isPopupEnabled } = props;
   
   const [isOpen, setIsOpen] = useState(false);
-
-  console.log('Popup isOpen', isOpen);
-
   
    useEffect(() => {
     const hasOpened = localStorage.getItem(PopupEnum.PopupShown);
+
     if (isPopupEnabled && !hasOpened) {
       setIsOpen(true);
       localStorage.setItem(PopupEnum.PopupShown, "true");
     }
-  }, []);
+  }, [isPopupEnabled]);
 
   return (
     <div className="popup-container">
       <Dialog open={isOpen} onOpenChange={setIsOpen}>
         <div className="popup-dialog">
 
-          {/* START POPUP - SAFE TO EDIT */}
+            <iframe className="iframe-desktop" src={`https://docs.google.com/document/d/e/2PACX-1vQBDy5XvY6XeXrLdG8AXXUUMpqkwfQDOgxXlByOj3H11zElOwlYg9jOJTPuqxmRcPAN8AZfych5m6Hh/pub?embedded=true&rand=${Date.now()}`}></iframe>
 
-          <h2 className="popup-title">Die Kinder- und Jugendarztpraxis Dr. med. Carmen Müller</h2>
-          
-          <p className="popup-date">
-            bleibt vom <span className="popup-highlight">20.06.</span> bis zum <span className="popup-highlight">04.07.2025</span> wegen Urlaub geschlossen.
-          </p>
-          
-          <p className="popup-date">
-            Ab dem <span className="popup-highlight">07.07.2025</span> sind wir wieder wie gewohnt für Sie da.
-          </p>
-          
-          <div className="popup-divider"></div>
-          
-          <p className="popup-subtitle">
-            In dringenden Fällen vertreten uns folgende Praxen:
-          </p>
-          
-          <div className="popup-columns">
-            <div className="popup-column">
-              <p className="popup-date-small">Am 20.06.25</p>
-              <p className="popup-doctor"><strong>Dres med. Duelli/Springer</strong></p>
-              <p className="popup-address">Keplerstraße 26</p>
-              <p className="popup-address">69120 Heidelberg</p>
-              <p className="popup-address">Tel: 480890</p>
-            </div>
-            
-            <div className="popup-column-divider"></div>
-            
-            <div className="popup-column gorning">
-              <p className="popup-date-small">Ab dem 23.06.–04.07.25</p>
-              <p className="popup-doctor"><strong>Dr. med. Gorning</strong></p>
-              <p className="popup-address">Brückenkopfstr. 1/2,</p>
-              <p className="popup-address">69120 Heidelberg</p>
-              <p className="popup-address">Tel: 401213</p>
-            </div>
-                        
-            <div className="popup-column">
-              <p className="popup-doctor haffelder"><strong>Dr. med. Haffelder</strong></p>
-              <p className="popup-address">Furtwänglerstraße 1</p>
-              <p className="popup-address">69121 Heidelberg</p>
-              <p className="popup-address">Tel: 470147</p>
-            </div>
-          </div>
-          
+            <iframe className="iframe-mobile" src={`https://docs.google.com/document/d/e/2PACX-1vSWjecDpNJrw-t1dKkH7yj8Y4DCEpgH2mvuH1DeI4yXmmqWb8Beu6OSvwOVPzDYiU07lF9Et_gu4i8n/pub?embedded=true&rand=${Date.now()}`}></iframe>
+
           <div className="popup-footer">
             <Button className="popup-close-button" onClick={() => setIsOpen(false)}>
               Schließen
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,15 +4,16 @@ import image2 from "../../img/praxis-gallery/praxis-2.jpeg";
 import image3 from "../../img/praxis-gallery/praxis-3.jpeg";
 
 import './HomePage.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Popup from "../../components/Popup/Popup";
 
 const HomePage = () => {
 
-  const SHOW_POPUP = true;
-
   const [clickTitleCount, setClickTitleCount] = useState(0);
   const [showCapy, setShowCapy] = useState(false);
+  const [popDesktopEnabled, setPopDesktopEnabled] = useState(false);
+  const [popMobileEnabled, setPopMobileEnabled] = useState(false);
+  const showPopup = popDesktopEnabled && popMobileEnabled;
 
   const handleTitleClick = () => {
     setClickTitleCount((prevCount) => prevCount + 1);
@@ -26,6 +27,31 @@ const HomePage = () => {
       setClickTitleCount(0);
     }
   };
+
+  useEffect(() => {
+    // DESKTOP DOCUMENT
+    fetch("https://docs.google.com/document/d/e/2PACX-1vQBDy5XvY6XeXrLdG8AXXUUMpqkwfQDOgxXlByOj3H11zElOwlYg9jOJTPuqxmRcPAN8AZfych5m6Hh/pub")
+      .then(() => {
+        console.log("DESKTOP DOCUMENT PUBLISHED");
+        setPopDesktopEnabled(true)   
+      })
+      .catch((error) => {
+        console.log('DESKTOP DOCUMENT ERROR', error);
+        setPopDesktopEnabled(false)     
+      });
+
+    // MOBILE DOCUMENT
+    fetch("https://docs.google.com/document/d/e/2PACX-1vSWjecDpNJrw-t1dKkH7yj8Y4DCEpgH2mvuH1DeI4yXmmqWb8Beu6OSvwOVPzDYiU07lF9Et_gu4i8n/pub")
+      .then(() => {
+        console.log("MOBILE DOCUMENT PUBLISHED");
+        setPopMobileEnabled(true)
+      })
+      .catch((error) => {
+        console.log('MOBILE DOCUMENT ERROR', error);
+        setPopMobileEnabled(false)
+      });
+  }, []);
+
 
   const images = [
     {
@@ -44,7 +70,7 @@ const HomePage = () => {
   
   return (
     <div className="page-content praxis-content">
-      <Popup isPopupEnabled={SHOW_POPUP} />
+      <Popup isPopupEnabled={showPopup} />
       <section className='praxis-image-gallery'>
         <ImageGallery 
             items={images}
